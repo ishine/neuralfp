3d899cb658ce1f2a18cb2939f5d405c138e545df
Aditya Bhattacharjee <aditya.bhattacharjee01@estudiant.upf.edu>
2021-06-04 14:46:57 +0200

    organized dataloading

--- modules/data.py
+++ modules/data.py
@@ -1,18 +1,21 @@
+import json
 import torch
 from torch import Tensor
 from torch.utils.data import Dataset
 import torchaudio
 import numpy as np
-# from pydub import AudioSegment
+from torchaudio.transforms import MelSpectrogram
 
 offset = 1.25
 SAMPLE_RATE = 8000
 
 class NeuralfpDataset(Dataset):
-    def __init__(self, path, transform):
+    def __init__(self, path, json_dir, transform):
         # self.dataset = dataset
         self.path = path
         self.transform = transform
+        with open(json_dir) as json_file:
+            self.filenames  = json.load(json_file)
         # self.input_shape = input_shape
         self.ignore_idx = []
         self.mixer = torchaudio.transforms.DownmixMono()
@@ -21,13 +24,13 @@ class NeuralfpDataset(Dataset):
         if idx in self.ignore_idx:
             return self[idx + 1]
         
-        path = self.path
-        audio, sr = torchaudio.load(path)
+        datapath = self.path + "/" + self.filenames[idx]
+        audio, sr = torchaudio.load(datapath)
         audioData = self.mixer(audio[0])
         audioData = audioData[::(int)(sr/SAMPLE_RATE)]     # Downsampling
         r = np.random.randint(0,len(audioData))
-        offset_time = sr*offset
-        audioData = audioData[r:r+offset_time]               # 1.2 second samples
+        offset_frame = sr*offset
+        audioData = audioData[r:r+offset_frame]               # 1.25 second samples
         
         # if audio.shape[1] < self.input_shape[1]:
         #     self.ignore_idx.append(idx)
@@ -36,9 +39,11 @@ class NeuralfpDataset(Dataset):
         if self.transform:
             audioData = self.transform(samples=audioData.numpy(),sample_rate=SAMPLE_RATE)
             audioData = torch.from_numpy(audioData)
-        return audioData
+            
+        specData = MelSpectrogram(sample_rate=SAMPLE_RATE, win_length=1024, hop_length=256, n_fft=512)
+        return specData
     
     def __len__(self):
-        return len(self.dataset)
+        return len(self.filenames)
             
     
