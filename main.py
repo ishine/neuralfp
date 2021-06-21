7a9a78ef1d56bad2ba9ff9dcc18b360ebb2ec64d
Aditya Bhattacharjee <aditya.bhattacharjee01@estudiant.upf.edu>
2021-06-21 20:29:52 +0200

    update main.py

--- main.py
+++ main.py
@@ -59,7 +59,8 @@ def train(train_loader, model, loss_fn, optimizer, criterion):
         # label = torch.arange(embeddings.size(0)/2)
         # labels = torch.cat([label,label], dim=0).to(device)
         loss = criterion(z_i, z_j)
-        print(h_i.shape)
+        print(torch.sum(h_i, dim =0))
+        print(torch.sum(h_i, dim =1))
         
         # if torch.count_nonzero(torch.isnan(loss)) > 0:
         #     print(z_i)
