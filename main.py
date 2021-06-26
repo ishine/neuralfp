56c7bc41ee241bae30e35b0994a969cccf032ea3
Aditya Bhattacharjee <aditya.bhattacharjee01@estudiant.upf.edu>
2021-06-27 01:04:40 +0200

    checkpoint fix

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ def train(train_loader, model, loss_fn, optimizer, criterion):
 
 def save_ckp(state,epoch):
     if not os.path.exists(model_folder): os.makedirs(model_folder)
-    torch.save(state, "{}/model_org_epoch_{].pth".format(model_folder,epoch))
+    torch.save(state, "{}/model_org_epoch_{}.pth".format(model_folder,epoch))
 
 def load_ckp(checkpoint_fpath, model, optimizer, scheduler):
     checkpoint = torch.load(checkpoint_fpath)
