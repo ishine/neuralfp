d9933b52318db6f179cc42123b5ebef9b1432176
Aditya Bhattacharjee <aditya.bhattacharjee01@estudiant.upf.edu>
2021-07-20 20:34:07 +0200

    commit testing pipeline

---
+++ create_query_data.py
@@ -0,0 +1,60 @@
+import random
+import numpy as np
+import librosa
+import soundfile as sf
+import os
+import json
+import uuid
+from audiomentations import Compose,Shift,PitchShift,TimeStretch,AddImpulseResponse,FrequencyMask,TimeMask,ClippingDistortion,AddBackgroundNoise,Gain
+
+
+root = os.path.dirname(__file__)
+
+validation_dir = os.path.join(root,'data/gtzan_2sec_2K')
+ir_dir = os.path.join(root,'data/ir_filters')
+noise_dir = os.path.join(root,'data/noise')
+data_dir = os.path.join(root,'data/genres')
+
+
+SAMPLE_RATE = 8000
+offset = 2
+
+ref = {}
+idx = 0
+for filename in os.listdir(data_dir):
+  if filename.endswith(".wav") or filename.endswith(".mp3"): 
+    ref[idx] = filename
+    idx += 1
+
+if not os.path.exists(validation_dir):
+  os.makedirs(validation_dir)
+with open('gtzan.json') as f:
+    ref = json.load(f)
+iters = 2000
+augment = Compose([
+    # Shift(min_fraction=-0.2, max_fraction=0.2, rollover=False),
+    # PitchShift(min_semitones=-2, max_semitones=2, p=0.5),
+    # TimeStretch(min_rate=0.8, max_rate=3, p=0.5),
+    AddImpulseResponse(ir_path=ir_dir, p=1),
+    # FrequencyMask(min_frequency_band=0.1, max_frequency_band=0.5,p=1),
+    # TimeMask(min_band_part=0.1, max_band_part=1),
+    ClippingDistortion(),
+    AddBackgroundNoise(sounds_path=noise_dir, min_snr_in_db=0, max_snr_in_db=10,p=1),
+    Gain(),
+    # Mp3Compression()
+    ])
+
+
+for i in range(iters):
+  r1 = random.randrange(1000)
+  fpath = os.path.join(data_dir, ref[r1])
+  audio, sr = librosa.load(fpath, sr=8000, mono=True)
+  if i % 50 == 0:
+    print(f"Step [{i}/{iters}]")
+  offset_frame = int(SAMPLE_RATE*offset)
+  r2 = np.random.randint(0,len(audio)-offset_frame)
+  audioData = audio[r2:r2+offset_frame]
+  augmented_samples = augment(samples=audioData, sample_rate=SAMPLE_RATE)
+  fname = ref[r1].split(".wav")[0] + "-" + str(uuid.uuid4()) + ".wav"
+  sf.write(os.path.join(validation_dir,fname), augmented_samples, SAMPLE_RATE)
+
