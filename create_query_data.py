f5e4100b1188ea3c9130b978f537fb75f395004b
Aditya Bhattacharjee <aditya.bhattacharjee01@estudiant.upf.edu>
2021-08-14 15:34:33 +0200

    minor bug fix test.py

--- create_query_data.py
+++ create_query_data.py
@@ -72,7 +72,7 @@ for offset in offset_list:
         # FrequencyMask(min_frequency_band=0.1, max_frequency_band=0.5,p=1),
         # TimeMask(min_band_part=0.1, max_band_part=1),
         ClippingDistortion(),
-        AddBackgroundNoise(sounds_path=noise_dir, min_snr_in_db=0, max_snr_in_db=10,p=1),
+        AddBackgroundNoise(sounds_path=noise_dir, min_snr_in_db=0, max_snr_in_db=7,p=1),
         # Gain(),
         # Mp3Compression()
         ])
