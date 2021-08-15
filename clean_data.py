f990a29a3411d01c8dba562fc9cb35f435bfab7a
Aditya Bhattacharjee <aditya.bhattacharjee01@estudiant.upf.edu>
2021-08-15 19:24:30 +0200

    minor bug fix test.py

---
+++ clean_data.py
@@ -0,0 +1,35 @@
+import os
+import librosa
+import random
+import json
+import warnings
+import shutil
+
+root = os.path.dirname(__file__)
+
+data_dir = data_dir = os.path.join(root,"data/test_data/fma_large")
+test_dir = os.path.join(root,"data/test_data/10K_subset")
+json_path = os.path.join(root,"data/fma_large.json")
+
+if not os.path.exists(test_dir):
+    os.mkdir(test_dir)
+    
+iters = 10000
+i = 0
+with open(json_path) as f:
+    ref = json.load(f)
+while i < iters:
+     r1 = random.randrange(len(os.listdir(data_dir)))
+     fpath = os.path.join(data_dir, ref[str(r1)])
+     try:
+         with warnings.catch_warnings():
+             warnings.simplefilter("ignore")
+             audio, sr = librosa.load(fpath, sr=8000, mono=True)
+     except Exception:
+         continue
+    
+     dst = os.path.join(test_dir,ref[str(r1)])
+     shutil.copy2(fpath, dst)
+     if i % 50 == 0:
+         print(f"Step [{i}/{iters}]")
+     i+=1
