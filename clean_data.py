a87fef0e26e1fa4361a1b6d91051b5f46881f948
Aditya Bhattacharjee <aditya.bhattacharjee01@estudiant.upf.edu>
2021-08-19 19:24:12 +0200

    test case

--- clean_data.py
+++ clean_data.py
@@ -58,7 +58,7 @@ json_path = os.path.join(root,"data/fma_large.json")
 
 dataset = {}
 idx = 0
-for filename in os.listdir(data_dir)[:55000]:
+for filename in os.listdir(data_dir)[:65000]:
   if filename.endswith(".wav") or filename.endswith(".mp3"): 
     dataset[idx] = filename
     idx += 1
