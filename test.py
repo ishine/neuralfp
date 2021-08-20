d0eb3e252bd402442fa556a1cf0cebc3d8fc11ee
Aditya Bhattacharjee <aditya.bhattacharjee01@estudiant.upf.edu>
2021-08-21 01:01:42 +0200

    test run

--- test.py
+++ test.py
@@ -177,7 +177,7 @@ def main():
             num_workers=0, pin_memory=True, drop_last=False)
         # print(next(iter(test_loader))[0].shape)
         ref_db = create_fp_db(test_loader, model)
-        torch.save(ref_db, os.path.join(fp_dir, args.test_dir.split('/')[-1] + "_ver2.pt"))
+        torch.save(ref_db, os.path.join(fp_dir, args.test_dir.split('/')[-1] + "_ver3.pt"))
 
         
         
@@ -198,7 +198,7 @@ def main():
             query_dataset, batch_size=1, shuffle=False,
             num_workers=0, pin_memory=True, drop_last=False)
         query_db = create_fp_db(query_loader, model)
-        torch.save(query_db, os.path.join(fp_dir, args.query_dir.split('/')[-1] + "_ver2.pt"))
+        torch.save(query_db, os.path.join(fp_dir, args.query_dir.split('/')[-1] + "_ver3.pt"))
         
         evaluate_hitrate(ref_db, query_db)
         
