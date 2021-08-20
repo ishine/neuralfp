d0ba16d183b6e0765802c6e7f70cc1c63e179c49
Aditya Bhattacharjee <aditya.bhattacharjee01@estudiant.upf.edu>
2021-08-20 17:40:32 +0200

    testing ir_conv

--- neuralfp/modules/transformations.py
+++ neuralfp/modules/transformations.py
@@ -34,22 +34,27 @@ class TransformNeuralfp:
             # Gain(),
             # Mp3Compression()
             ])
-    def irconv(self, x):
+    def irconv(self, x, p):
         ir_dir = self.ir_dir
-        r1 = random.randrange(len(os.listdir(ir_dir)))
-        fpath = os.path.join(ir_dir, os.listdir(ir_dir)[r1])
-        x_ir, fs = librosa.load(fpath, sr=None)
-        fftLength = np.maximum(len(x), len(x_ir))
-        X = np.fft.fft(x, n=fftLength)
-        X_ir = np.fft.fft(x_ir, n=fftLength)
-        x_aug = np.fft.ifft(np.multiply(X_ir, X))[0:len(x)].real
-        if np.max(np.abs(x_aug)) == 0:
-            pass
-        else:
-            x_aug = x_aug / np.max(np.abs(x_aug))  # Max-normalize
+        if random.random() < p:
+            r1 = random.randrange(len(os.listdir(ir_dir)))
+            fpath = os.path.join(ir_dir, os.listdir(ir_dir)[r1])
+            x_ir, fs = librosa.load(fpath, sr=None)
+            fftLength = np.maximum(len(x), len(x_ir))
+            X = np.fft.fft(x, n=fftLength)
+            X_ir = np.fft.fft(x_ir, n=fftLength)
+            x_aug = np.fft.ifft(np.multiply(X_ir, X))[0:len(x)].real
+            if np.max(np.abs(x_aug)) == 0:
+                print("divide by zero")
+                pass
+            else:
+                x_aug = x_aug / np.max(np.abs(x_aug))  # Max-normalize
+        
+        else: 
+            x_aug = x
         
         return x_aug.astype(np.float32)
             
     def __call__(self, x_i, x_j):
-        x_j = self.irconv(x_j)
+        x_j = self.irconv(x_j, p=0.8)
         return self.train_transform_i(x_i, sample_rate=self.sample_rate), self.train_transform_j(x_j, sample_rate=self.sample_rate)
