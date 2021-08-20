724012562c261a77f2131841bdfb036a7c61b1d9
Aditya Bhattacharjee <aditya.bhattacharjee01@estudiant.upf.edu>
2021-08-20 23:59:55 +0200

    SI_conv initial commit

--- neuralfp/modules/encoder.py
+++ neuralfp/modules/encoder.py
@@ -36,12 +36,12 @@ class Encoder(nn.Module):
         for channels in architecture:
             
             if si_cnn:
-                layers.append(SI_Conv(in_channels=in_channels, out_channels=channels, kernel_size=[1,kernel_size], stride=[1,stride], padding=[0,1]))
+                layers.append(SI_Conv(in_channels=in_channels, out_channels=channels, kernel_size=[kernel_size,kernel_size], stride=[stride,stride], padding=[1,1]))
                 shape[0] = channels
                 shape[2] = int(np.ceil(shape[2]/2))
-                layers.append(nn.LayerNorm(shape))
-                layers.append(nn.ReLU())
-                layers.append(SI_Conv(in_channels=in_channels, out_channels=channels, kernel_size=[kernel_size,1], stride=[stride,1], padding=[1,0]))
+                # layers.append(nn.LayerNorm(shape))
+                # layers.append(nn.ReLU())
+                # layers.append(SI_Conv(in_channels=in_channels, out_channels=channels, kernel_size=[kernel_size,1], stride=[stride,1], padding=[1,0]))
                 shape[1] = int(np.ceil(shape[1]/2))
                 layers.append(nn.LayerNorm(shape))
                 layers.append(nn.ReLU())
