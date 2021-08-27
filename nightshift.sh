6e02eede363d6cadfcf5fe9e09d6e69a745c0aa2
Aditya Bhattacharjee <aditya.bhattacharjee01@estudiant.upf.edu>
2021-08-27 02:53:06 +0200

    test run

--- nightshift.sh
+++ nightshift.sh
@@ -7,6 +7,6 @@ python create_query_data.py --length 10 --test_dir=data/test_data/fma_large --no
 # python test.py --fp_path=fingerprints/fma_large_ver3.pt --model_path=model/model_ver3_epoch_260.pth --query_dir=data/fma_large_3sec_2K --eval=True >> results/fma_large_3sec_2K.txt
 # python test.py --fp_path=fingerprints/fma_large_ver3.pt --model_path=model/model_ver3_epoch_260.pth --query_dir=data/fma_large_5sec_2K --eval=True >> results/fma_large_5sec_2K.txt
 # python test.py --fp_path=fingerprints/fma_large_ver3.pt --model_path=model/model_ver3_epoch_260.pth --query_dir=data/fma_large_6sec_2K --eval=True >> results/fma_large_6sec_2K.txt
-python test.py --fp_path=fingerprints/fma_large_ver3.pt --model_path=model/model_ver5_epoch_230.pth --query_dir=data/fma_large_10sec_2K --eval=True
+python test.py --fp_path=fingerprints/fma_large_ver5.pt --model_path=model/model_ver5_epoch_230.pth --query_dir=data/fma_large_10sec_2K --eval=True
 # python main.py >> results/train_ver4.txt
 
