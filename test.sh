6d23a55ba71436199f02186fc8ad044553e9c096
Aditya Bhattacharjee <aditya.bhattacharjee01@estudiant.upf.edu>
2021-08-27 09:13:35 +0200

    test run

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-python main.py >> results/train_ver5.txt
-python clean_data.py
-rm -rf data/fma_large_*
-
+python test.py --fp_path=fingerprints/fma_large_ver5.pt --model_path=model/model_ver5_epoch_230.pth --query_dir=data/fma_large_2sec_2K --eval=True >> results/fma_large_2sec_2K.txt
+python test.py --fp_path=fingerprints/fma_large_ver5.pt --model_path=model/model_ver5_epoch_230.pth --query_dir=data/fma_large_3sec_2K --eval=True >> results/fma_large_3sec_2K.txt
+python test.py --fp_path=fingerprints/fma_large_ver5.pt --model_path=model/model_ver5_epoch_230.pth --query_dir=data/fma_large_5sec_2K --eval=True >> results/fma_large_5sec_2K.txt
+python test.py --fp_path=fingerprints/fma_large_ver5.pt --model_path=model/model_ver5_epoch_230.pth --query_dir=data/fma_large_6sec_2K --eval=True >> results/fma_large_6sec_2K.txt
 
 
