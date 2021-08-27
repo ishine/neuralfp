3bbdae654ba73d937e1cabbc3bb35d5159c50a69
Aditya Bhattacharjee <aditya.bhattacharjee01@estudiant.upf.edu>
2021-08-28 00:16:03 +0200

    validation run

--- nightshift.sh
+++ nightshift.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-python test.py --test_dir=data/test_data/fma_large --model_path=model/model_ver5_epoch_230.pth
-python create_query_data.py --length 2 3 5 6 10 --test_dir=data/test_data/fma_large --noise_dir=data/Noises_unsampled --ir_dir=data/IR_unsampled
+python test.py --test_dir=data/test_data/fma_large --model_path=model/model_ver7_epoch_100.pth
+python create_query_data.py --length 11 --test_dir=data/test_data/fma_large --noise_dir=data/Noises_unsampled --ir_dir=data/IR_unsampled
 # python create_query_data.py --length 10 --test_dir=data/test_data/fma_large --noise_dir=data/Noises_unsampled --ir_dir=data/IR_unsampled
 # python test.py --fp_path=fingerprints/fma_large_ver3.pt --model_path=model/model_ver3_epoch_260.pth --query_dir=data/fma_large_1sec_2K --eval=True >> results/fma_large_1sec_2K.txt
 # python test.py --fp_path=fingerprints/fma_large_ver3.pt --model_path=model/model_ver3_epoch_260.pth --query_dir=data/fma_large_2sec_2K --eval=True >> results/fma_large_2sec_2K.txt
 # python test.py --fp_path=fingerprints/fma_large_ver3.pt --model_path=model/model_ver3_epoch_260.pth --query_dir=data/fma_large_3sec_2K --eval=True >> results/fma_large_3sec_2K.txt
 # python test.py --fp_path=fingerprints/fma_large_ver3.pt --model_path=model/model_ver3_epoch_260.pth --query_dir=data/fma_large_5sec_2K --eval=True >> results/fma_large_5sec_2K.txt
 # python test.py --fp_path=fingerprints/fma_large_ver3.pt --model_path=model/model_ver3_epoch_260.pth --query_dir=data/fma_large_6sec_2K --eval=True >> results/fma_large_6sec_2K.txt
-python test.py --fp_path=fingerprints/fma_large_ver5.pt --model_path=model/model_ver5_epoch_230.pth --query_dir=data/fma_large_10sec_2K --eval=True >> results/fma_large_10sec_2K.txt
+python test.py --fp_path=fingerprints/fma_large_ver5.pt --model_path=model/model_ver7_epoch_100.pth --query_dir=data/fma_large_11sec_2K --eval=True
 # python main.py >> results/train_ver4.txt
 
