9f85f3ac6df114bfb24a6779dee3e014279930f0
Aditya Bhattacharjee <aditya.bhattacharjee01@estudiant.upf.edu>
2021-08-28 07:04:18 +0200

    validation run

--- nightshift.sh
+++ nightshift.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-python test.py --test_dir=data/test_data/fma_large --model_path=model/model_ver230_epoch_7.pth
+# python test.py --test_dir=data/test_data/fma_large --model_path=model/model_ver230_epoch_7.pth
 python create_query_data.py --length 10 --test_dir=data/test_data/fma_large --noise_dir=data/Noises_unsampled --ir_dir=data/IR_unsampled
 # python create_query_data.py --length 10 --test_dir=data/test_data/fma_large --noise_dir=data/Noises_unsampled --ir_dir=data/IR_unsampled
 # python test.py --fp_path=fingerprints/fma_large_ver3.pt --model_path=model/model_ver3_epoch_260.pth --query_dir=data/fma_large_1sec_2K --eval=True >> results/fma_large_1sec_2K.txt
