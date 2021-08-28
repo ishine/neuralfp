eb7fe944a548e0d461b93cbccf015fd323181f75
Aditya Bhattacharjee <aditya.bhattacharjee01@estudiant.upf.edu>
2021-08-28 20:11:38 +0200

    validation run

--- nightshift.sh
+++ nightshift.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# python test.py --test_dir=data/test_data/fma_large --model_path=model/model_ver230_epoch_7.pth
+python test.py --test_dir=data/test_data/fma_large --model_path=model/model_ver6_epoch_300.pth
 python create_query_data.py --length 10 --test_dir=data/test_data/fma_large --noise_dir=data/Noises_unsampled --ir_dir=data/IR_unsampled
 # python create_query_data.py --length 10 --test_dir=data/test_data/fma_large --noise_dir=data/Noises_unsampled --ir_dir=data/IR_unsampled
 # python test.py --fp_path=fingerprints/fma_large_ver3.pt --model_path=model/model_ver3_epoch_260.pth --query_dir=data/fma_large_1sec_2K --eval=True >> results/fma_large_1sec_2K.txt
@@ -7,6 +7,6 @@ python create_query_data.py --length 10 --test_dir=data/test_data/fma_large --no
 # python test.py --fp_path=fingerprints/fma_large_ver3.pt --model_path=model/model_ver3_epoch_260.pth --query_dir=data/fma_large_3sec_2K --eval=True >> results/fma_large_3sec_2K.txt
 # python test.py --fp_path=fingerprints/fma_large_ver3.pt --model_path=model/model_ver3_epoch_260.pth --query_dir=data/fma_large_5sec_2K --eval=True >> results/fma_large_5sec_2K.txt
 # python test.py --fp_path=fingerprints/fma_large_ver3.pt --model_path=model/model_ver3_epoch_260.pth --query_dir=data/fma_large_6sec_2K --eval=True >> results/fma_large_6sec_2K.txt
-python test.py --fp_path=fingerprints/fma_large_ver7.pt --model_path=model/model_ver230_epoch_7.pth --query_dir=data/fma_large_10sec_2H --eval=True
+python test.py --fp_path=fingerprints/fma_large_ver6.pt --model_path=model/model_ver6_epoch_300.pth --query_dir=data/fma_large_10sec_5H --eval=True
 # python main.py >> results/train_ver4.txt
 
