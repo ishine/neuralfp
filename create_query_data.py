fff54abaab9337aa4aee7c7e2ca05841c49332e6
Aditya Bhattacharjee <aditya.bhattacharjee01@estudiant.upf.edu>
2021-08-29 15:16:19 +0200

    validation run

--- create_query_data.py
+++ create_query_data.py
@@ -76,7 +76,7 @@ def irconv(ir_dir, x, p):
 for offset in offset_list:
     SAMPLE_RATE = 8000
     
-    validation_dir = os.path.join(root,'data/fma_large_'+str(offset)+'sec_ps_'+str(args.ps))
+    validation_dir = os.path.join(root,'data/fma_large_'+str(offset)+'sec_ps_0')
     # validation_dir = os.path.join(root,'data/eval_test')
         
     
@@ -87,7 +87,7 @@ for offset in offset_list:
     iters = 2000
     augment = Compose([
         # Shift(min_fraction=-0.2, max_fraction=0.2, rollover=False),
-        PitchShift(min_semitones=args.ps, max_semitones=args.ps, p=0.8),
+        # PitchShift(min_semitones=args.ps, max_semitones=args.ps, p=0.8),
         # TimeStretch(min_rate=0.8, max_rate=3, p=0.5),
         # AddImpulseResponse(ir_path=ir_dir, p=1),
         # FrequencyMask(min_frequency_band=0.1, max_frequency_band=0.5,p=1),
