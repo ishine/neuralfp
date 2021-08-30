18d8fe5acf3838417b30c910a0c498964a7323f0
Aditya Bhattacharjee <aditya.bhattacharjee01@estudiant.upf.edu>
2021-08-30 14:31:19 +0200

    test run

--- test.sh
+++ test.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-python create_query_data.py --length 10 --test_dir=data/test_data/fma_large --noise_dir=data/Noises_unsampled --ir_dir=data/IR_unsampled --ts=1.1
-python create_query_data.py --length 10 --test_dir=data/test_data/fma_large --noise_dir=data/Noises_unsampled --ir_dir=data/IR_unsampled --ts=1.2
-python create_query_data.py --length 10 --test_dir=data/test_data/fma_large --noise_dir=data/Noises_unsampled --ir_dir=data/IR_unsampled --ts=1.3
-python create_query_data.py --length 10 --test_dir=data/test_data/fma_large --noise_dir=data/Noises_unsampled --ir_dir=data/IR_unsampled --ts=0.9
-python create_query_data.py --length 10 --test_dir=data/test_data/fma_large --noise_dir=data/Noises_unsampled --ir_dir=data/IR_unsampled --ts=0.8
-python create_query_data.py --length 10 --test_dir=data/test_data/fma_large --noise_dir=data/Noises_unsampled --ir_dir=data/IR_unsampled --ts=0.7
-
-# python test.py --fp_path=fingerprints/fma_large_ver6.pt --model_path=model/model_ver6_epoch_280.pth --query_dir=data/fma_large_10sec_ps_1 --eval=True >> results/ver6_ps_1.txt
+# python create_query_data.py --length 10 --test_dir=data/test_data/fma_large --noise_dir=data/Noises_unsampled --ir_dir=data/IR_unsampled --ts=1.1
+python test.py --fp_path=fingerprints/fma_large_ver8.pt --model_path=model/model_ver8_epoch_240.pth --query_dir=data/fma_large_10sec_ts_1.1 --eval=True >> results/ver8_ts_1.1.txt
+python test.py --fp_path=fingerprints/fma_large_ver8.pt --model_path=model/model_ver8_epoch_240.pth --query_dir=data/fma_large_10sec_ts_1.2 --eval=True >> results/ver8_ts_1.2.txt
+python test.py --fp_path=fingerprints/fma_large_ver8.pt --model_path=model/model_ver8_epoch_240.pth --query_dir=data/fma_large_10sec_ts_1.3 --eval=True >> results/ver8_ts_1.3.txt
+python test.py --fp_path=fingerprints/fma_large_ver8.pt --model_path=model/model_ver8_epoch_240.pth --query_dir=data/fma_large_10sec_ts_0.9 --eval=True >> results/ver8_ts_0.9.txt
+python test.py --fp_path=fingerprints/fma_large_ver8.pt --model_path=model/model_ver8_epoch_240.pth --query_dir=data/fma_large_10sec_ts_0.8 --eval=True >> results/ver8_ts_0.8.txt
+python test.py --fp_path=fingerprints/fma_large_ver8.pt --model_path=model/model_ver8_epoch_240.pth --query_dir=data/fma_large_10sec_ts_0.7 --eval=True >> results/ver8_ts_0.7.txt
 
 
 
