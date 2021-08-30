5c9405643809b5fc2219f66029f1e52a29d16291
Aditya Bhattacharjee <aditya.bhattacharjee01@estudiant.upf.edu>
2021-08-31 01:33:30 +0200

    test run

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ def main():
     train_dataset = NeuralfpDataset(path=data_dir, json_dir=json_dir, transform=TransformNeuralfp(ir_dir=ir_dir, noise_dir=noise_dir,sample_rate=8000))
     train_loader = torch.utils.data.DataLoader(
         train_dataset, batch_size=batch_size, shuffle=True,
-        num_workers=4, pin_memory=True, drop_last=True)
+        num_workers=8, pin_memory=True, drop_last=True)
     
     model = Neuralfp(encoder=encoder.Encoder())
     model = nn.DataParallel(model).to(device)
