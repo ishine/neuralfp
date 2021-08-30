fe7bf83935a76f671e3cd6f70c80008eaeac5b8d
Aditya Bhattacharjee <aditya.bhattacharjee01@estudiant.upf.edu>
2021-08-30 04:23:25 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
-# python create_query_data.py --length 10 --test_dir=data/test_data/fma_large --noise_dir=data/Noises_unsampled --ir_dir=data/IR_unsampled --ps=1
-python test.py --fp_path=fingerprints/fma_large_ver6.pt --model_path=model/model_ver6_epoch_280.pth --query_dir=data/fma_large_10sec_ps_1 --eval=True >> results/ver6_ps_1.txt
-python test.py --fp_path=fingerprints/fma_large_ver6.pt --model_path=model/model_ver6_epoch_280.pth --query_dir=data/fma_large_10sec_ps_2 --eval=True >> results/ver6_ps_2.txt
-python test.py --fp_path=fingerprints/fma_large_ver6.pt --model_path=model/model_ver6_epoch_280.pth --query_dir=data/fma_large_10sec_ps_-1 --eval=True >> results/ver6_ps_-1.txt
-python test.py --fp_path=fingerprints/fma_large_ver6.pt --model_path=model/model_ver6_epoch_280.pth --query_dir=data/fma_large_10sec_ps_-2 --eval=True >> results/ver6_ps_-2.txt
-python test.py --fp_path=fingerprints/fma_large_ver6.pt --model_path=model/model_ver6_epoch_280.pth --query_dir=data/fma_large_10sec_ps_-3 --eval=True >> results/ver6_ps_-3.txt
+python create_query_data.py --length 10 --test_dir=data/test_data/fma_large --noise_dir=data/Noises_unsampled --ir_dir=data/IR_unsampled --ts=1.1
+python create_query_data.py --length 10 --test_dir=data/test_data/fma_large --noise_dir=data/Noises_unsampled --ir_dir=data/IR_unsampled --ts=1.2
+python create_query_data.py --length 10 --test_dir=data/test_data/fma_large --noise_dir=data/Noises_unsampled --ir_dir=data/IR_unsampled --ts=1.3
+python create_query_data.py --length 10 --test_dir=data/test_data/fma_large --noise_dir=data/Noises_unsampled --ir_dir=data/IR_unsampled --ts=0.9
+python create_query_data.py --length 10 --test_dir=data/test_data/fma_large --noise_dir=data/Noises_unsampled --ir_dir=data/IR_unsampled --ts=0.8
+python create_query_data.py --length 10 --test_dir=data/test_data/fma_large --noise_dir=data/Noises_unsampled --ir_dir=data/IR_unsampled --ts=0.7
+
+# python test.py --fp_path=fingerprints/fma_large_ver6.pt --model_path=model/model_ver6_epoch_280.pth --query_dir=data/fma_large_10sec_ps_1 --eval=True >> results/ver6_ps_1.txt
+
 
 
 
